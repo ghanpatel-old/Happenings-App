d92b4bf2d82cc6863fd1e5732d8c5245aa07188b
Ghan <ghanpatel.gmail.com>
2012-02-02 00:47:54 -0500

    Express, SSH Key, ready for first Heroku push

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var http = require("http");
+var express = require('express');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
