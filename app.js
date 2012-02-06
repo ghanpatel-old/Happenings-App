ecc7aa38d9fdfb19ac8979baa5fc9ee39ea14781
Ghan <ghanpatel.gmail.com>
2012-02-06 00:24:18 -0500

    connect to mongohq server

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = module.exports = express.createServer(express.logger());
 
-// connect to Mongo when the app initializes
-mongoose.connect('mongodb://ghanpatel@gmail.com:hFib11235@staff.mongohq.com:10010/app2729959');
+
+mongoose.connect('mongodb://heroku:1111@staff.mongohq.com:10010/app2729959');
 
 app.configure(function(){
   app.use(express.bodyParser());
@@ -13,7 +13,6 @@ app.configure(function(){
   app.use(app.router);
 });
 
-// set up the RESTful API, handler methods are defined in api.js
 var api = require('./controllers/api.js');
 app.post('/thread', api.post);
 app.get('/thread/:title.:format?', api.show);
